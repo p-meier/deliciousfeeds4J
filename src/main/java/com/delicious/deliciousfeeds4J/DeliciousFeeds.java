b0de80d094e82213f29fda283adb399bb92e48d0
Patrick Meier <private@github.com>
2013-02-07 11:27:32 +0100

    - Reduced Java-Version to 1.6 in userAgent-String - constrainAPILimit now defaults to false

--- src/main/java/com/delicious/deliciousfeeds4J/DeliciousFeeds.java
+++ src/main/java/com/delicious/deliciousfeeds4J/DeliciousFeeds.java
@@ -1,17 +1,11 @@
 /*
  * Copyright (c) 2013 by Patrick Meier.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 
 package com.delicious.deliciousfeeds4J;
@@ -46,11 +40,11 @@ public class DeliciousFeeds {
     //Some default values
     public static final int DEFAULT_COUNT = 10;
 
-    public static final String DEFAULT_USER_AGENT = "deliciousfeeds4j Java/1.7";
+    public static final String DEFAULT_USER_AGENT = "deliciousfeeds4j Java/1.6";
 
     public static final boolean DEFAULT_EXPAND_URLS = false;
 
-    public static final boolean DEFAULT_CONTRAIN_API_LIMIT = true;
+    public static final boolean DEFAULT_CONTRAIN_API_LIMIT = false;
 
     //The API Endpoint
     private static final String API_ENDPOINT = "http://feeds.delicious.com/v2/json/";
